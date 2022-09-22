211173b49e877f264e52ef21d908277b3007b524
unknown <faroukadedamola1@gmail.com>
2022-09-22 21:38:41 +0100

    initial commit
    commiting files for the initial version of slider- review

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import { FaQuoteRight } from "react-icons/fa";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+
+import data from "./data";
+import "./index.css";
 
 function App() {
+  const [people, setPeople] = useState(data);
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    let lastSlide = people.length - 1;
+    if (value < 0) {
+      setValue(lastSlide);
+    }
+    if (value > lastSlide) {
+      setValue(0);
+    }
+  }, [value, people]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setValue(value + 1);
+    }, 5000);
+    return () => {
+      clearInterval(slider);
+    };
+  }, [value]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <section className="section">
+        <header>
+          <h1>
+            <span>/</span> reviews
+          </h1>
+        </header>
+        <div className="section-center">
+          {people.map((person, personIndex) => {
+            const { image, name, title, quote, id } = person;
+            let position = "nextSlide";
+            if (personIndex === value) {
+              position = "activeSlide";
+            }
+            if (
+              personIndex === value - 1 ||
+              (value === 0 && personIndex === people.length - 1)
+            ) {
+              position = "lastSlide";
+            }
+            return (
+              <article className={position} key={id}>
+                <img src={image} alt={name} className="person-img" />
+                <h4>{name}</h4>
+                <p className="title">{title}</p>
+                <p className="text">{quote}</p>
+                <FaQuoteRight className="icon" />
+              </article>
+            );
+          })}
+        </div>
+        <button
+          type="button"
+          className="prev"
+          onClick={() => setValue(value - 1)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <FiChevronRight />
+        </button>
+        <button
+          className="next"
+          type="button"
+          onClick={() => setValue(value + 1)}
+        >
+          <FiChevronLeft />
+        </button>
+      </section>
+    </>
   );
 }
 
